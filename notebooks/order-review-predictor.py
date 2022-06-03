ab1008c342e49779c5944bbfdff04f4ae0c52dea
Admin Admin <admin@hopsworks.ai>
2022-06-03 04:55:04 +0000

    Add sklearn transformer to predictor

--- notebooks/order-review-predictor.py
+++ notebooks/order-review-predictor.py
@@ -8,9 +8,14 @@ class Predict(object):
         """Prepare and load a trained model"""
         # NOTE: The env var ARTIFACT_FILES_PATH contains the path to the model artifact files
         
+        # load the sklearn pipeline object
+        self.sktransformer = joblib.load(os.environ["ARTIFACT_FILES_PATH"] + "/minmax-transformer.pkl")
         # load the trained model
         self.model = joblib.load(os.environ["ARTIFACT_FILES_PATH"] + "/review.pkl")
 
     def predict(self, inputs):
-        """Serve prediction using a trained model""" # Numpy Arrays are note JSON serializable
-        return self.model.predict(inputs).tolist()
+        """Serve prediction using a trained model"""
+        # transform inputs with minmax scalar transformer
+        input = self.sktransformer.transform(inputs)
+        # make prediction
+        return self.model.predict(inputs).tolist()  # Numpy Arrays are note JSON serializable
