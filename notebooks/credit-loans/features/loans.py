7e31c8fae98f4e5f4b14ba01c7e27ed56e4a3958
Jim Dowling <jim@logicalclocks.com>
2023-03-06 17:29:40 +0100

    add credit example

---
+++ notebooks/credit-loans/features/loans.py
@@ -0,0 +1,58 @@
+# +
+import pandas as pd
+import datetime
+import numpy as np
+
+def home_ownership(df : pd.DataFrame)-> pd.DataFrame:
+    df.loc[(df.home_ownership == 'ANY') | (df.home_ownership == 'NONE'), 'home_ownership'] = 'OTHER'
+    return df
+    
+
+
+# -
+
+def home_ownership_udf(home_ownership: str)-> str:
+    if (home_ownership == 'ANY' or home_ownership == 'NONE') :
+        return 'OTHER'
+    return home_ownership
+
+
+def pub_rec(number):
+    if number == 0.0:
+        return 0
+    else:
+        return 1
+
+
+def mort_acc(number):
+    if number == 0.0:
+        return 0
+    elif number >= 1.0:
+        return 1
+    else:
+        return number
+
+
+def pub_rec_bankruptcies(number):
+    if number == 0.0:
+        return 0
+    elif number >= 1.0:
+        return 1
+    else:
+        return number
+
+
+def fill_mort_acc(total_acc, mort_acc, total_acc_avg):
+    if np.isnan(mort_acc):
+        return total_acc_avg[total_acc].round()
+    else:
+        return mort_acc
+
+
+# on demand feature
+def extract_zipcode(address):
+    return address[-5:]
+
+
+def extract_year(earliest_cr_line):
+    return earliest_cr_line.year
